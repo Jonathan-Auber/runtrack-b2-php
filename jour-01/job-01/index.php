21922430e97dbe45ca9ce98d7d61faec163983da
Jonathan.Auber <auber.jonathan@gmail.com>
2023-09-12 14:32:45 +0200

    job-01

---
+++ jour-01/job-01/index.php
@@ -0,0 +1,18 @@
+<?php
+
+function my_str_search(string $haystack, string $needle): int
+{
+    $length = 0;
+    while (isset($haystack[$length])) {
+        $length++;
+    }
+    $occurences = 0;
+    for ($i = 0; $i < $length; $i++) {
+        if ($haystack[$i] === $needle) {
+            $occurences++;
+        }
+    }
+    return $occurences;
+}
+
+echo my_str_search('La Plateforme', 'a');
